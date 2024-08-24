Fixed random feature map scaling, added 2 different beta temps, and cleaned up code

model.py
@@ -11,13 +11,13 @@ import numpy as np
 def sim(x, y):
     D = x.shape[-1]
     if len(x.shape) == 1 and len(y.shape) == 1:
-        return (x @ y.conj()).real
+        return (x @ y.conj()).real / D
     elif len(x.shape) == 2 and len(y.shape) == 1:
-        return torch.einsum("ij,j->i", x, y.conj()).real
+        return torch.einsum("ij,j->i", x, y.conj()).real / D
     elif len(x.shape) == 1 and len(y.shape) == 2:
-        return torch.einsum("j,ij->i", x, y.conj()).real
+        return torch.einsum("j,ij->i", x, y.conj()).real / D
     else:
-        return torch.einsum("ri,ci->rc", x, y.conj()).real
+        return torch.einsum("ri,ci->rc", x, y.conj()).real / D
 
 class RandomFeatureMap(torch.nn.Module):
     def __init__(self, in_dim, out_dim, alpha=1):
@@ -27,7 +27,6 @@ class RandomFeatureMap(torch.nn.Module):
         self.alpha = alpha                  # inverse length scale
         self.W = torch.nn.Parameter(torch.randn(out_dim, in_dim).to(torch.complex64) / alpha, requires_grad=False)
         #self.W = torch.nn.Parameter((torch.rand(out_dim, in_dim).to(torch.complex64) * 2 * np.pi / alpha))    # sinc kernel
-        self.sqrt_out_dim = np.sqrt(out_dim)
 
     # Applies the random feature map.
     # Input shape: [in_dim]
@@ -36,16 +35,12 @@ class RandomFeatureMap(torch.nn.Module):
     # Output shape: [..., out_dim]
     def forward(self, x):
         if len(x.shape) == 1:
-            #out = torch.exp(1j * self.W @ x.to(torch.complex64) / self.alpha) / self.sqrt_out_dim
-            # n = torch.norm(x, p=2, dim=-1, keepdim=True)
-            n = 1 
-            out = torch.exp(1j * self.W @ (x/n).to(torch.complex64) / self.alpha) / self.sqrt_out_dim
-            #return out/torch.norm(out, p=2)
+            out = torch.exp(1j * self.W @ x.to(torch.complex64) / self.alpha)
             return out
         else:
             # n = torch.norm(x, p=2, dim=-1, keepdim=True)
             n = 1
-            out = torch.exp(1j * torch.einsum("ij,...j->...i", self.W, (x/n).to(torch.complex64)) / self.alpha ) / self.sqrt_out_dim
+            out = torch.exp(1j * torch.einsum("ij,...j->...i", self.W, (x/n).to(torch.complex64)) / self.alpha )
             # out = torch.exp(1j * ((x/n).to(torch.complex64) @ self.W.T) / self.alpha ) / self.sqrt_out_dim
             #return out/torch.norm(out, p=2, dim=-1, keepdim=True)
             return out
@@ -58,7 +53,8 @@ class POCML(torch.nn.Module):
                  state_dim,
                  random_feature_dim,
                  alpha=1,
-                 beta=1,
+                 beta_obs=1,
+                 beta_state=1,
                  memory_bypass=False,
                  decay=0.9,
                  memory=None,
@@ -70,7 +66,8 @@ class POCML(torch.nn.Module):
         self.n_actions = n_actions # number of actions
         self.state_dim = state_dim # dimension of state space
         self.random_feature_dim = random_feature_dim # dimension of random feature map output
-        self.beta = beta # temperature parameter for softmax
+        self.beta_obs = beta_obs # temperature parameter for predicting observation
+        self.beta_state = beta_state # temperature parameter for predicting state (after clean up)
         self.memory_bypass = memory_bypass # whether to bypass memory; bypassed memory will directly use \phi(Q) as memory
         self.decay = decay # decay parameter for memory
 
@@ -126,25 +123,12 @@ class POCML(torch.nn.Module):
         return F.softmax(score, dim=0)
     
     def get_obs_score_from_memory(self, state):
-        return self.beta * (self.M.conj().T @ state).real
-    
-    # Cleans up state to be a linear combination of the columns of \phi(Q) (Eq. 19).
-    # Optional: pass in state obtained from associative memory given observation, weighted by c.
-    # c needs to be in [0, 1]
-    # Returns weights in linear combination.
-    # def clean_up(self, state, state_from_memory=None, c=0):
-    #     phi_Q = self.random_feature_map(self.Q.T).T
-    #     weights = F.softmax(self.beta * (phi_Q.conj().T @ state).real, dim=0)
-    #     if state_from_memory is not None:
-    #         memory_weights = F.softmax(self.beta * (phi_Q.conj().T @ state_from_memory).real, dim=0)
-    #         weights = (1-c) * weights + c * memory_weights # convex combination
-    #     new_state = phi_Q @ weights.to(torch.complex64)
-    #     self.state = new_state
-    #     return weights
+        return self.beta_obs * sim(self.M.T, state)
 
     def get_state_score(self, state):
         phi_Q = self.get_state_kernel()
-        return self.beta * (phi_Q.conj().T @ state).real
+        #print(phi_Q.shape, state.shape)
+        return self.beta_state * sim(phi_Q.T, state)
 
     def clean_up(self, state):
         phi_Q = self.get_state_kernel()
@@ -277,5 +261,5 @@ class POCML(torch.nn.Module):
         return phi_Q.detach()
         
     def get_memory_kernel(self):
-        return (self.M.T @ self.M.conj()).real.detach()
+        return sim(self.M.T, self.M)
 

trainer.py
@@ -66,7 +66,8 @@ class POCMLTrainer(CMLTrainer):
         self.norm = norm
 
         # POCML model param for training
-        self.beta = model.beta                                  # state prediction temperature, eq(21)
+        self.beta_obs = model.beta_obs                          # observation prediction temperature
+        self.beta_state = model.beta_state                      # state prediction temperature
         self.alpha = model.random_feature_map.alpha             # (inverse) lengscale of the RBF
         self.lr_Q_o = lr_Q_o
         self.lr_V_o = lr_V_o
@@ -92,7 +93,7 @@ class POCMLTrainer(CMLTrainer):
         diff_s_squared_norm = torch.sum(diff_s ** 2, dim=-1)
 
         # Approximating K with phi(Q)
-        # K = ((model.random_feature_map(model.Q.T)) @ (model.random_feature_map(s_hat).T)).real
+        # K = ((model.random_feature_map(model.Q.T)) @ (model.random_feature_map(s_hat).T)).real / D
 
         # Compute exact K 
         K = torch.exp(-self.alpha * diff_s_squared_norm)
@@ -101,8 +102,8 @@ class POCMLTrainer(CMLTrainer):
         # Soft-collapsing 
         # K = F.softmax(K * 10, dim=0)
 
-        print("K_ij at time \n", self.model.t, ":\n", K)
-        print("s_diff_n2:", diff_s_squared_norm)
+        # print("K_ij at time \n", self.model.t, ":\n", K)
+        # print("s_diff_n2:", diff_s_squared_norm)
 
         self.update_tensor = torch.einsum('j,ij,ijm->ijm', w_hat, K, diff_s)
 
@@ -198,7 +199,7 @@ class POCMLTrainer(CMLTrainer):
                     state_pred_mem = model.compute_weights(hd_state_pred_mem)       # eq (25)  u^tilde}_{t+1}
 
                     # weight computation, (32), w_hat_k are columns of w_hat
-                    w_tilde = model.compute_weights(model.M)                        # assume one-hot encoding of x_t
+                    w_tilde = model.compute_weights(model.M.T)                        # assume one-hot encoding of x_t
 
                     if tt == 0 and ttt == 0: 
                         print("oh_o_next_pred", oh_o_next_pred)
@@ -240,7 +241,7 @@ class POCMLTrainer(CMLTrainer):
     def __update_Q_o(self, oh_o_next_pred, oh_o_next_target):
 
         # TODO scaling factor for update (29)
-        eta = self.lr_Q_o * self.alpha * self.beta * self.lr_all
+        eta = self.lr_Q_o * self.alpha * self.beta_obs * self.lr_all
         
         update_weight = eta * torch.einsum('k, kmn', oh_o_next_target - oh_o_next_pred, self.Z_q)
 
@@ -250,7 +251,7 @@ class POCMLTrainer(CMLTrainer):
     def __update_V_o(self, oh_o_next_pred, oh_o_next_target, oh_a):
 
         # TODO scaling factor for update (30)
-        eta = self.lr_V_o * self.alpha * self.beta * self.lr_all
+        eta = self.lr_V_o * self.alpha * self.beta_obs * self.lr_all
 
         update_weight = eta * torch.einsum('k, kmn', oh_o_next_target - oh_o_next_pred, self.Z_v)
 
@@ -259,7 +260,7 @@ class POCMLTrainer(CMLTrainer):
         
     def __update_Q_s(self, state_pred_bind, state_pred_mem):
 
-        eta = self.lr_Q_s * self.alpha * self.beta * self.lr_all
+        eta = self.lr_Q_s * self.alpha * self.beta_state * self.lr_all
 
         update_weight = eta * torch.einsum('k, kmn', state_pred_mem - state_pred_bind, self.W_q)
 
@@ -268,87 +269,9 @@ class POCMLTrainer(CMLTrainer):
 
     def __update_V_s(self, state_pred_bind, state_pred_mem, oh_a):
 
-        eta = self.lr_V_s * self.alpha * self.beta * self.lr_all
+        eta = self.lr_V_s * self.alpha * self.beta_state * self.lr_all
 
         update_weight = eta * torch.einsum('k, kmn', state_pred_mem - state_pred_bind, self.W_v)
         
         self.model.V += update_weight
         return update_weight
-
-
-
-    # # Create tensor reused in the update rule (30 - 33)
-    # # the tensor U is of shape n_s * n_s * n where U[i,j,k] = \omega_{i,j,k} K_{i，j} (s^{hat}_j - s_i) 
-    # Deprecated: This function is replaced by __prep_update 
-    # def __prep_update_old(self, w_tilde, w_hat, oh_a):
-        
-    #     model = self.model 
-    #     Q = model.Q
-
-    #     omega = torch.outer(w_tilde, w_hat)                 # omega_ij = w_tilde_i * w_hat_j
-
-    #     v_t = model.V @ oh_a
-    #     s_hat = (Q.T) + v_t[None, :]                        # s_hat_j = (s_j + v_t)
-    #     diff_s = (Q.T)[:, None, :] - s_hat[None, :, :]      # generate n_s * n_s * n tensor [s_i - s_hat_j]_i,j
-        
-    #     diff_s_squared_norm = torch.sum(diff_s ** 2, dim=-1)
-
-    #     # Approximating K with phi(Q)
-    #     # K = ((model.random_feature_map(model.Q.T)) @ (model.random_feature_map(s_hat).T)).real
-
-    #     # Compute exact K 
-    #     K = torch.exp(-self.alpha * diff_s_squared_norm)
-    #     # Soft-collapsing 
-    #     # K = F.softmax(K * 10, dim=0)
-
-    #     print("K_ij at time \n", self.model.t, ":\n", K)
-    #     print("s_diff_n2:", diff_s_squared_norm)
-
-    #     self.update_tensor = (omega * K)[:, : ,None] * diff_s
-    #     #self.update_tensor = torch.einsum('ij,ijm->ijm', omega * K, diff_s)    # Alternatively
-
-    # def __update_Q_o_old(self, oh_o_next_pred, oh_o_next_target, add=True):
-
-    #     eta = self.lr_Q_o * self.alpha * self.beta * self.lr_all
-        
-    #     u = torch.eye(self.model.n_states).to(self.device)        # TODO double check if this can be optimized
-    #     if add:
-    #         update_weight = eta * (1 - torch.dot(oh_o_next_pred, oh_o_next_target)) * \
-    #                         torch.einsum('ijk,jl->kl', self.update_tensor, u)
-    #         self.model.Q += update_weight
-    #     else:
-    #         update_weight = eta * (1 - torch.dot(oh_o_next_pred, oh_o_next_target)) * \
-    #                         torch.einsum('ijk,il->kl', self.update_tensor, u)
-    #         self.model.Q -= update_weight
-    #     return update_weight
-
-    # def __update_V_o_old(self, oh_o_next_pred, oh_o_next_target, oh_a):
-
-    #     eta = self.lr_V_o * self.alpha * self.beta * self.lr_all
-    #     update_weight = eta * (1 - torch.dot(oh_o_next_pred, oh_o_next_target)) * \
-    #                     torch.einsum('ijk,l->kl', self.update_tensor, oh_a)
-    #     self.model.V += update_weight
-    #     return update_weight 
-        
-    # def __update_Q_s_old(self, state_pred_bind, add=True):
-
-    #     eta = self.lr_Q_s * self.alpha * self.beta * self.lr_all
-       
-    #     u = torch.eye(self.model.n_states).to(self.device)        # TODO double check if this can be optimized
-    #     if add:
-    #         update_weight = eta * \
-    #                         torch.einsum('i,ijk,jl->kl', state_pred_bind, self.update_tensor, u)
-    #         self.model.Q += update_weight
-    #     else:
-    #         update_weight = eta * \
-    #                         torch.einsum('i,ijk,il->kl', state_pred_bind, self.update_tensor, u)
-    #         self.model.Q -= update_weight
-    #     return update_weight
-
-    # def __update_V_s_old(self, state_pred_bind, oh_a):
-
-    #     eta = self.lr_V_s * self.alpha * self.beta * self.lr_all
-    #     update_weight = eta * \
-    #                     torch.einsum('i,ijk,l->kl', state_pred_bind, self.update_tensor, oh_a)
-    #     self.model.V += update_weight
-    #     return update_weight