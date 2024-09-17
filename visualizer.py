c5109fe38c9b4e143481900db26bd403b4c6b337
zouzhuowen <zhuowez1@uci.edu>
2024-09-16 17:29:09 -0700

    Add batch visualization

--- visualizer.py
+++ visualizer.py
@@ -1,12 +1,76 @@
-from sklearn.manifold import MDS
+from sklearn.manifold import * 
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import wandb
 
-def visualize(distances, legend:str ="Node", title:str = "Graph Visualization using MDS", log = False):
-    mds = MDS(n_components =2)
-    positions = mds.fit_transform(distances)
+def batch_visualize(distances, legend:str ="Node", methods = "mds", log = False):
+    """
+    Visualize pairwise distances using various dimensionality reduction methods (e.g., MDS, t-SNE).
+
+    Args:
+        distances: A matrix of pairwise distances.
+        legend (str): The label to use for legends in the visualization (default: "Node").
+        methods (str or list or dict): Specifies which (methods : arguments) to use for visualization.
+                              - It can be a single string (e.g., "mds"), or a list of method names; arguments are padded empty up to constraints from the methods.
+                              - If set to "all", it will automatically include all implemented methods.
+        log (bool): Whether to log the process.
+    """
+    mtds = methods
+
+    if isinstance(mtds, str):
+        mtds = [mtds]
+    
+    if isinstance(mtds, list):
+        mtds = {item: {} for item in mtds}
+
+    if "all" in mtds:
+        for mtd in ["mds", "tsne", "isomap", "lle", "se"]:
+            if mtd not in mtds: 
+                mtds[mtd] = {}
+
+    print(mtds)
+
+    if ("mds" in mtds): 
+        ml = MDS(**mtds["mds"])
+        visualize(distances, ml, legend = legend, method_name = "MDS", log = log)
+
+    if ("tsne" in mtds): 
+        if "perplexity" not in mtds["tsne"]:
+            mtds["tsne"]["perplexity"] = min(len(distances)-1, 5)          # TODO: recommend #actions for state visualization
+        
+        ml = TSNE(**mtds["tsne"])
+        visualize(distances, ml, legend = legend, method_name = "tSNE", log = log)
+
+    # Isomap
+    if "isomap" in mtds:
+
+        if "n_neighbors" not in mtds["isomap"]:
+            mtds["isomap"]["n_neighbors"] = min(len(distances)-1, 5)          # TODO
+
+        ml = Isomap(**mtds["isomap"])
+        visualize(distances, ml, legend=legend, method_name="Isomap", log=log)
+
+    # Locally Linear Embedding (LLE)
+    if "lle" in mtds:
+
+        if "n_neighbors" not in mtds["lle"]:
+            mtds["lle"]["n_neighbors"] = min(len(distances)-1, 5)          # TODO
+    
+        ml = LocallyLinearEmbedding(**mtds["lle"])
+        visualize(distances, ml, legend=legend, method_name="LLE", log=log)
+
+    # Spectral Embedding (SE)
+    if "se" in mtds:
+        # if "n_components" not in mtds["se"]:
+        #     mtds["se"]["n_neighbors"] = min(len(distances)-1, 2)          # TODO
+    
+        ml = SpectralEmbedding(**mtds["se"], affinity = "precomputed")
+        visualize(distances, ml, legend=legend, method_name="Spectral Embedding", log=log)
+
+def visualize(distances, ml = MDS(), legend:str ="Node", method_name = "MDS", log = False):
+
+    positions = ml.fit_transform(distances)
 
     fig, ax = plt.subplots(figsize=(8, 6))
     ax.scatter(positions[:, 0], positions[:, 1])
@@ -15,7 +79,9 @@ def visualize(distances, legend:str ="Node", title:str = "Graph Visualization us
     for i, (x, y) in enumerate(positions):
         ax.text(x, y, f'{legend} {i}', fontsize=12)
 
-    ax.set_title('Graph Visualization using MDS')
+    title = f'{legend} Visualization using {method_name}'
+
+    ax.set_title(f'{legend} Visualization using {method_name}')
     ax.set_xlabel('Dim. 1')
     ax.set_ylabel('Dim. 2')
     ax.grid(True)
